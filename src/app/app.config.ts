d65977172c2692094f8d5eff3dc2376241e17841
erick <erick.gonzalez13@utp.ac.pa>
2024-05-09 07:29:32 -0500

    firebase configuration

---
+++ src/app/app.config.ts
@@ -0,0 +1,109 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { provideFirebaseApp, initializeApp, getApp } from '@angular/fire/app';
+import {
+  provideAuth,
+  initializeAuth,
+  browserSessionPersistence,
+  indexedDBLocalPersistence,
+  browserPopupRedirectResolver,
+  connectAuthEmulator,
+} from '@angular/fire/auth';
+import {
+  provideFirestore,
+  initializeFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { providePerformance, getPerformance } from '@angular/fire/performance';
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from '@angular/fire/storage';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar'; //
+import { environment } from 'src/environments/environment';
+import { routes } from './app.routes';
+
+const useEmulators = environment.firebase;
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAnalytics(() => getAnalytics()),
+      provideAuth(() => {
+        const auth = initializeAuth(getApp(), {
+          persistence: useEmulators
+            ? browserSessionPersistence
+            : indexedDBLocalPersistence,
+          popupRedirectResolver: browserPopupRedirectResolver,
+        });
+
+        if (useEmulators) {
+          connectAuthEmulator(auth, `http://localhost:9099`, {
+            disableWarnings: true,
+          });
+        }
+
+        return auth;
+      }),
+      provideFirestore(() => {
+        const firestore = initializeFirestore(getApp(), {
+          experimentalForceLongPolling: useEmulators ? true : false,
+        });
+
+        if (useEmulators) {
+          connectFirestoreEmulator(firestore, 'localhost', 8080);
+        }
+
+        return firestore;
+      }),
+      provideFunctions(() => {
+        const functions = getFunctions();
+
+        if (useEmulators) {
+          connectFunctionsEmulator(functions, 'localhost', 5001);
+        }
+
+        return functions;
+      }),
+      provideMessaging(() => getMessaging()), // not using emulators for messages
+      providePerformance(() => getPerformance()), // not using emulators for checking app performance
+      provideStorage(() => {
+        const storage = getStorage();
+
+        if (useEmulators) {
+          connectStorageEmulator(storage, 'localhost', 9199);
+        }
+
+        return storage;
+      })
+    ),
+    ScreenTrackingService,
+    UserTrackingService,
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        duration: 4000,
+      },
+    },
+  ],
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  firebase : {
-    apiKey: "AIzaSyBmdt0JuWJsLjfrBdq86UrLQDsJLZAKKCQ",
-    authDomain: "playtokeys.firebaseapp.com",
-    databaseURL: "https://playtokeys-default-rtdb.firebaseio.com",
-    projectId: "playtokeys",
-    storageBucket: "playtokeys.appspot.com",
-    messagingSenderId: "253962828780",
-    appId: "1:253962828780:web:68b0ab44dd5c9be65f9ec9"
-  }
+  useEmulators: false,
+  firebase: {
+    apiKey: 'AIzaSyBmdt0JuWJsLjfrBdq86UrLQDsJLZAKKCQ',
+    authDomain: 'playtokeys.firebaseapp.com',
+    databaseURL: 'https://playtokeys-default-rtdb.firebaseio.com',
+    projectId: 'playtokeys',
+    storageBucket: 'playtokeys.appspot.com',
+    messagingSenderId: '253962828780',
+    appId: '1:253962828780:web:68b0ab44dd5c9be65f9ec9',
+  },
 };
