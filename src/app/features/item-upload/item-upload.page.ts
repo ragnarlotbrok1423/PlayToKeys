7957eaa22af7b79cdbd5320514a24b19fc36409e
erick <erick.gonzalez13@utp.ac.pa>
2024-05-17 21:26:26 -0500

    design complete

--- src/app/features/item-upload/item-upload.page.ts
+++ src/app/features/item-upload/item-upload.page.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonicModule} from "@ionic/angular";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-item-upload',
   templateUrl: './item-upload.page.html',
   styleUrls: ['./item-upload.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule, RouterLink]
 })
 export class ItemUploadPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { return 0
   }
 
 }
